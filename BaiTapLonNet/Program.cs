46f1f91b1513bdc1894618261f99b1b1c877de57
Duong Van Tuan <duongvantuan372@gmail.com>
2023-05-16 11:31:35 +0700

    16/05/2023

--- BaiTapLonNet/Program.cs
+++ BaiTapLonNet/Program.cs
@@ -33,7 +33,10 @@ app.UseSession();
 
 app.UseAuthentication();
 app.UseAuthorization();
-
+app.MapControllerRoute(
+      name: "areas",
+      pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+    );
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
